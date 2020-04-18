512be10c9e40cbee3d0a8db02ba0c48890d3fb57
andro <androzz1125@gmail.com>
2020-04-18 13:52:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Pixel Art Maker Project
+
+## Table of Contents
+
+* [Build](#build)
+* [Instructions](#instructions)
+* [Dependencies](#Dependencies)
+
+
+## Build
+
+* This game is built using HTML, CSS and JavaScript.
+* It runs on any web browser.
+* It needs internet connection to gather required files and run properly.
+
+
+## Instructions
+
+* To start the project, run the index.html file in a browser.
+* User can type in desired height, width and color to make a customized grid canvas.
+* Click on individual grid pixels to fill the selected color with selected color
+* Erase the color in individual grid pixels by double-click
+* To change the grid, resubmit with desired inputs.
+
+
+## Dependencies
+
+* Google Fonts
+
+
+
+
---
+++ designs.js
@@ -0,0 +1,42 @@
+//loop over to build grid
+function makeGrid(row, column) {
+    const $canvas = document.getElementById("pixelCanvas");
+    $canvas.innerHTML = "";
+    for (let i = 0; i <= row; i ++) {
+        let newRow = $canvas.insertRow();
+        for (let j = 0; j <= column; j ++) {
+            let newCell = newRow.insertCell();
+            newCell.onclick = fillColor;
+            newCell.ondblclick = eraseColor;
+        }
+    }
+}
+
+//build default grid
+makeGrid(20,20);
+
+//get size input and build customized grid
+document.getElementById("sizePicker").addEventListener("submit", function(event){
+    event.preventDefault();
+    let row = document.getElementById("inputHeight").value;
+    let column = document.getElementById("inputWidth").value;
+    makeGrid(row, column);
+});    
+
+//fill color
+function fillColor() {
+    const favColor = document.getElementById("colorPicker").value;
+    this.style.background = favColor;
+}
+
+//erase color
+function eraseColor() {
+    this.style.background = '#fff'
+}
+
+
+
+
+
+
+
